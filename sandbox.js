ce0e38c37a05396a78ca9bd67f0152f532f37809
Yasin Konyalı <yasinkonyali12@gmail.com>
2023-03-13 17:18:28 +0300

    Initial commit

---
+++ sandbox.js
@@ -0,0 +1,67 @@
+// //get an element by ID
+// const title = document.getElementById('page-title');
+// console.log(title);
+
+// //get an elements by their class name
+// const errors = document.getElementsByClassName('error');
+// console.log(errors);
+
+// //get an elements by their tag name
+// const paras = document.getElementsByTagName('p');
+// console.log(paras);
+
+//ADDING & CHANGING CONTENT
+
+// const para = document.querySelector('p');
+
+// // console.log(para.innerText);
+// // para.innerText = "Hello Mars!";
+
+// const paras = document.querySelectorAll('p');
+
+// // paras.forEach(para =>{
+// //     console.log(para.innerText);
+// //     para.innerText += "new text";
+// // });
+
+// const content = document.querySelector('.content');
+
+// // console.log(content.innerHTML);
+// // content.innerHTML = '<h2>This is the new H2.</h2>';
+
+// const people = ["ali", "osman", "hakan"];
+
+// people.forEach(person => {
+//     content.innerHTML += `<p>${person}</p>`;
+// });
+
+//GETTING & SETTING ATTRIBUTES
+
+// const link = document.querySelector('a');
+
+// console.log(link.getAttribute('href'));
+// link.setAttribute('href', 'https://www.twitter.com');
+// link.innerText = "Twitter";
+
+// const mssg = document.querySelector('p');
+
+// console.log(mssg.getAttribute('class'));
+// mssg.setAttribute('class', 'success');
+// mssg.setAttribute('style', 'color: red;');
+
+//CHANGING CSS STYLES
+
+// const title = document.querySelector('h1');
+
+// // title.setAttribute('style', 'margin: 50px;');
+
+// console.log(title.style);
+// console.log(title.style.color);
+
+// title.style.margin = '50px';
+// title.style.color = 'crimson';
+// title.style.fontSize = 80px;
+// title.style.margin = '';
+
+//ADDING & REMOVING CLASSES
+
